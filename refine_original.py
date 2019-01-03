3319338afd3e9b403cf26f56f978c98619bcb78c
Ziqi Pang <pangziqi@pku.edu.cn>
2019-01-03 11:49:10 +0800

    Update refine_original.py

--- refine_original.py
+++ refine_original.py
@@ -53,14 +53,17 @@ def run():
     """
     run the model
     """
-    epoch = 100
+    epoch = 50
     batch_size = 40
     os.environ["CUDA_VISIBLE_DEVICES"] = "2"
-    lr = 1e-3
+    lr = 5 * 1e-4
+    session = 5
 
-    unet = UNet().cuda()
-    criterion = nn.MSELoss()
-    optimizer = optim.SGD(unet.parameters(), lr, momentum = 0.05)
+    unet = UNet(useBN=True).cuda()
+    unet.load_state_dict(torch.load('/home/pzq/pose_warp/trained_model/bg_s4e47.pkl'))
+    criterion = nn.L1Loss()
+    advLoss = nn.BCELoss()
+    optimizer = optim.SGD(unet.parameters(), lr, momentum = 0.2)
 
     height = 128
     width = 64
@@ -116,19 +119,16 @@ def run():
                 for j in range(instance_num):
                     batch_gt[instance_num * i + j, :, :, :] = tgt_img
                     batch_in[instance_num * i + j, 3, :, :] = tgt_pose
-                    batch_in[instance_num * i + j, :3, :, :] = warped_imgs[index, j, :, :, :]
+                    batch_in[instance_num * i + j, :3, :, :] = warped_imgs[index, j, :, :, :] * tgt_mask \
+                                                               + (1 - tgt_mask) * tgt_img
                     batch_mask[instance_num * i + j, :, :] = tgt_mask
 
-            for c in range(3):
-                batch_gt[:, c, :, :] = batch_gt[:, c, :, :] * batch_mask[:, :, :]
             #print("End of prepare")
 
             # Start training
-            batch_in = batch_in
-            batch_gt = batch_gt
             optimizer.zero_grad()
-            batch_in = torch.tensor(batch_in.reshape((batch_size, 4, width, height))).float().cuda()
-            batch_gt = torch.tensor(batch_gt.reshape((batch_size, 3, width, height))).float().cuda()
+            batch_in = torch.tensor(batch_in).float().cuda()
+            batch_gt = torch.tensor(batch_gt).float().cuda()
             out = unet(batch_in)
             loss = criterion(out, batch_gt)
             loss.backward()
@@ -139,11 +139,91 @@ def run():
 
         loss_change.append(total_loss)
         print('Epoch [{e}] has loss of [{loss}]'.format(e = epo, loss = total_loss))
-        np.savetxt('loss_change.txt', np.array(loss_change))
+        np.savetxt('loss_change{s}.txt'.format(s=session), np.array(loss_change))
 
-        if epo % 3 == 0:
-            torch.save(unet.state_dict(), '/home/pzq/pose_warp/pretrain/{epo}.pkl'.format(epo=epo))
+        if (epo + 1) % 5 == 0:
+            torch.save(unet.state_dict(), '/home/pzq/pose_warp/pretrain/s{s}e{epo}.pkl'.format(s=session,epo=epo))
+
+
+def test_model():
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    unet = UNet(useBN=True).cuda()
+    unet.load_state_dict(torch.load('/home/pzq/pose_warp/pretrain/s5e49.pkl'))
+
+    width = 64
+    height = 128
+
+    src_dir = '/home/pzq/warp_data/img/market/'
+    pose_map_dir = '/home/pzq/warp_data/img/market_pose_map/'
+    tgt_mask_dir = '/home/pzq/warp_data/img/market_tgt_mask/'
+    warp_dir = '/home/pzq/warp_data/img/market_warped/'
+    match_path = '/home/pzq/warp_data/pose/market/match.json'
+    match_file = open(match_path, 'r')
+    data_json_path = '/home/pzq/warp_data/pose/market/market-final.json'
+    data_file = open(data_json_path, 'r')
+    match = json.load(match_file)
+    data = json.load(data_file)
+
+    print("Loading Done. Start perquisite job.")
+    mapping = pre.construct_mapping(data_json_path)
+    src_imgs = pre.read_color(data_json_path, width, height, src_dir, session_num=1000) / 255
+    print('Read in src images done')
+    pose_map = pre.read_gray(data_json_path, width, height, pose_map_dir, session_num=1000) / 255
+    tgt_masks = pre.read_gray(data_json_path, width, height, tgt_mask_dir, session_num=1000) / 255
+    print('Gray images reading done')
+    warped_imgs = pre.read_morphed_img_and_group(warp_dir, data_json_path, match_path, width, height, num=2, session_num=1000) / 255
+    print('Warped images reading done')
+
+    img_num = 1000
+    train_list = np.arange(0, img_num)
+    batch_size = 40
+    instance_num = 2  # 4 people warping for one id
+    batch_img_num = int(batch_size / instance_num)  # people num in batch
+    batch_num = int(img_num / batch_img_num)  # batch number in an epoch
+
+    np.random.shuffle(train_list)
+
+    # prepare the data
+    batch_in = np.zeros((batch_size, 4, height, width))
+    batch_gt = np.zeros((batch_size, 3, height, width))
+    batch_mask = np.zeros((batch_size, height, width))
+    # print("Start prepare")
+    for i in range(batch_img_num):
+        index = train_list[i]
+        tgt_img = src_imgs[index]
+        tgt_pose = pose_map[index]
+        tgt_mask = tgt_masks[index]
+
+        for j in range(instance_num):
+            batch_gt[instance_num * i + j, :, :, :] = tgt_img
+            batch_in[instance_num * i + j, 3, :, :] = tgt_pose
+            #batch_in[instance_num * i + j, :3, :, :] = warped_imgs[index, j, :, :, :] * tgt_mask \
+            #                                           + (1 - tgt_mask) * tgt_img
+            batch_mask[instance_num * i + j, :, :] = tgt_mask
+            batch_in[instance_num * i + j, :3, :, :] = warped_imgs[index, j, :, :, :]
+    # print("End of prepare")
+
+    # Start testing
+    batch_in = torch.tensor(batch_in).float().cuda()
+    with torch.no_grad():
+        out = unet(batch_in)
+
+    out = (np.array(out.detach().cpu()) * 255).astype(np.uint8)
+    batch_gt = (batch_gt * 255).astype(np.uint8)
+    batch_in = (np.array(batch_in.detach().cpu()) * 255).astype(np.uint8)
+
+    # Output the results
+    out_dir = '/home/pzq/pose_warp/vis_data/'
+    for i in range(batch_size):
+        img_syn = np.transpose(out[i], (1, 2, 0))
+        img_tgt = np.transpose(batch_gt[i], (1, 2, 0))
+        img_src = np.transpose(batch_in[i], (1, 2, 0))
+        cv2.imwrite(out_dir + 's' + str(i).zfill(2) + '.jpg', img_syn)
+        cv2.imwrite(out_dir + 't' + str(i).zfill(2) + '.jpg', img_tgt)
+        cv2.imwrite(out_dir + 'o' + str(i).zfill(2) + '.jpg', img_src)
 
 
 if __name__ == '__main__':
     run()
+    # test_model()
+
