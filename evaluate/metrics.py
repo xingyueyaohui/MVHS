2a1475594323490ed29cc245bb753af194731239
Ziqi Pang <pangziqi@pku.edu.cn>
2019-01-09 11:35:07 +0800

    Add files via upload

---
+++ evaluate/metrics.py
@@ -0,0 +1,12 @@
+"""
+This file is for evaluating the performance
+Including the metrics of:
+    SSIM
+    Inception Score(to be)
+    Identity Loss(to be)
+"""
+def compute_avg_ssim():
+    return None
+
+def copmute_best_ssim():
+    return None
