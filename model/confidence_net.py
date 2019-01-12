e5a71296b4550169c73f316658c85011f9d2980f
Ziqi Pang <pangziqi@pku.edu.cn>
2019-01-12 23:08:56 +0800

    Create confidence_net.py

---
+++ model/confidence_net.py
@@ -0,0 +1,95 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+"""
+Model for confidence net
+Initially try the UNet structure
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as functional
+
+
+def add_conv_stage(dim_in, dim_out, kernel_size=3, stride=1, padding=1, bias=True, useBN=False):
+    if useBN:
+        return nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
+            nn.BatchNorm2d(dim_out),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(dim_out, dim_out, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
+            nn.BatchNorm2d(dim_out),
+            nn.LeakyReLU(0.1)
+        )
+    else:
+        return nn.Sequential(
+            nn.Conv2d(dim_in, dim_out, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
+            nn.ReLU(),
+            nn.Conv2d(dim_out, dim_out, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
+            nn.ReLU()
+        )
+
+
+def add_merge_stage(ch_coarse, ch_fine, in_coarse, in_fine, upsample):
+    conv = nn.ConvTranspose2d(ch_coarse, ch_fine, 4, 2, 1, bias=False)
+    torch.cat(conv, in_fine)
+
+    return nn.Sequential(
+        nn.ConvTranspose2d(ch_coarse, ch_fine, 4, 2, 1, bias=False)
+    )
+    up_sample(in_coarse)
+
+
+def upsample(ch_coarse, ch_fine):
+    return nn.Sequential(
+        nn.ConvTranspose2d(ch_coarse, ch_fine, 4, 2, 1, bias=False),
+        nn.ReLU()
+    )
+
+
+class ConfidenceNet(nn.Module):
+    def __init__(self,useBN=False):
+        super(ConfidenceNet, self).__init__()
+
+        self.conv1 = add_conv_stage(3, 32, useBN=useBN)
+        self.conv2 = add_conv_stage(32, 64, useBN=useBN)
+        self.conv3 = add_conv_stage(64, 128, useBN=useBN)
+        self.conv4 = add_conv_stage(128, 256, useBN=useBN)
+        self.conv5 = add_conv_stage(256, 512, useBN=useBN)
+
+        self.conv0 = nn.Sequential(
+            nn.Conv2d(32, 1, 3, 1, 1),
+            nn.Sigmoid()
+        )
+
+        self.max_pool = nn.MaxPool2d(2)
+
+        self.upsample54 = upsample(512, 256)
+        self.upsample43 = upsample(256, 128)
+        self.upsample32 = upsample(128, 64)
+        self.upsample21 = upsample(64, 32)
+
+        ## weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                if m.bias is not None:
+                    m.bias.data.zero_()
+                if m.bias is not None:
+                    nn.init.xavier_uniform(m.weight)
+
+    def forward(self, x):
+        conv1_out = self.conv1(x)
+        conv2_out = self.conv2(self.max_pool(conv1_out))
+        conv3_out = self.conv3(self.max_pool(conv2_out))
+        conv4_out = self.conv4(self.max_pool(conv3_out))
+        conv5_out = self.conv5(self.max_pool(conv4_out))
+
+        tconv5_out = self.upsample54(conv5_out)
+        tconv4_out = self.upsample43(tconv5_out)
+        tconv3_out = self.upsample32(tconv4_out)
+        tconv2_out = self.upsample21(tconv3_out)
+
+        conv0_out = self.conv0(tconv2_out)
+
+        return conv0_out
